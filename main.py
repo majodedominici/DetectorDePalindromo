bb7bef6293c759b873e98beb1eb61f137f0d93ea
María José Dedominici <mjdedominici@gmail.com>
2021-09-05 17:29:57 -0300

    Primera parte del programa

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 ## La consigna del trabajo es realizar un detector de palíndromos, es decir, que la palabra se lea tanto de izquierda a
 # derecha y de, derecha a izquierda por igual
+
+igual, aux = 0, 0
+texto=input("Ingrese la palabra que desea evaluar: ")
+
+for ind in reversed(range(0, len(texto))):
+  if texto[ind].lower() == texto[aux].lower():
+    igual += 1
+  aux += 1
